4070bc461f97d6081862ea50feb42aa6d3777895
W1729608 <W1729608@apps.losrios.edu>
2022-09-07 10:20:14 -0700

    Addded three dots

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -15,7 +15,12 @@ int main()
 	RenderWindow window(vm, "ChaosGame", Style::Default);
 
 	RectangleShape point1(Vector2f(2, 2));
-	point1.setPosition(249, 249);
+	RectangleShape point2(Vector2f(2, 2));
+	RectangleShape point3(Vector2f(2, 2));
+	point1.setPosition(50, 50);
+	point2.setPosition(100, 50);
+	point3.setPosition(75, 100);
+
 
 	while (window.isOpen())
 	{
@@ -44,6 +49,8 @@ int main()
 		****************************************
 		*/
 		window.draw(point1);
+		window.draw(point2);
+		window.draw(point3);
 
 		// Show everything we just drew
 		window.display();
